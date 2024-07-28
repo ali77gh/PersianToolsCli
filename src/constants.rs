46a5301580f31a72820daa39455bf6b4bdea85df
Ali Ghahremani <alighahremani1377@gmail.com>
2024-07-28 23:07:44 +0330

    fix: constants values

--- src/constants.rs
+++ src/constants.rs
@@ -1,4 +1,4 @@
 pub const INVALID: &str = "invalid";
-pub const ERROR: &str = "invalid";
-pub const NOT_FOUND: &str = "invalid";
+pub const ERROR: &str = "error";
+pub const NOT_FOUND: &str = "not found";
 pub const SEPARATOR: &str = ",";
